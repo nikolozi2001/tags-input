d1c510d4a991c489c8f55d1c38f2cb74cf919d2e
nikolozi2001 <mr.qachibaia44@gmail.com>
2024-08-18 17:43:22 +0400

    Working with methods

--- src/components/TagsInput.vue
+++ src/components/TagsInput.vue
@@ -1,19 +1,36 @@
 <template>
-    <div v-for="(tag, index) in tags">{{ index + ' : ' + tag }}</div>
-    <hr>
+    <div v-for="(tag, index) in tags">{{ index + " : " + tag }}
+        <a href="#" @click.prevent="removeTag(index)">&times;</a>
+    </div>
+    <hr />
     {{ newTag }}
-    <input type="text" v-model="newTag" @keydown.enter="tags.push(newTag)" @keydown.tab.prevent="tags.push(newTag)" />
-    <!-- <button @click="tags.push(newTag)">OK</button> -->
+    <input type="text" v-model="newTag" @keydown.enter="addNewTag" @keydown.delete="removeLastTag"
+        @keydown.tab.prevent="addNewTag" />
 </template>
 
 <script>
-
 export default {
     data: () => ({
         tags: ["vue", "react", "angular"],
-        newTag: "preact "
+        newTag: "preact ",
     }),
+
+    methods: {
+        addNewTag() {
+            if (this.newTag) {
+                this.tags.push(this.newTag.trim());
+                this.newTag = "";
+            }
+        },
+        removeTag(index) {
+            this.tags.splice(index, 1);
+
+        },
+        removeLastTag() {
+            if (this.newTag.length === 0) {
+                this.removeTag(this.tags.length - 1);
+            }
+        }
+    },
 };
-
-
 </script>
