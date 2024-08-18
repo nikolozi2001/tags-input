4f12a5fc81b8270163cd364cd142a45e6625f444
nikolozi2001 <mr.qachibaia44@gmail.com>
2024-08-18 11:41:41 +0400

    single file component

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import TagsInput from "./components/TagsInput.vue";
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.component("tags-input", TagsInput)
+
+app.mount("#app");
